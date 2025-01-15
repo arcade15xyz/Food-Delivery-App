476000b4e32caff87a5b0e1374cb7b2e2eded6d8
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-15 11:51:16 +0530

    Added a custom hook namely useHttp hook and avoided common error while loading by using dependencies in useCallback and useEffect

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,26 +1,20 @@
-import { useState,useEffect } from "react";
+
 import MealItem from "./MealItem";
+import useHttp from "../Hooks/useHttp";
 
-
+const requestConfig = {};
 
 export default function Meals() {
-  const [loadedMeals,setLoadedMeals]=useState([]);
 
-  useEffect(()=>{
-    async function fetchMeals() {
-      const response = await fetch('http://localhost:3000/meals');
-  
-      if(!response.ok){
-        // ...
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-  
-    fetchMeals();
-  },[]);
+  const { 
+      data: loadedMeals,
+      isLoading,
+      error 
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-
+  if (isLoading){
+    return( <p>Data is Loading</p>)
+  }
 
   return(
     <ul id="meals">
